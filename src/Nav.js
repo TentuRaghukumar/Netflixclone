80788d0d2c0e8ae4744939d98cd212cba763e1b9
Raghukumar Tentu <tentu@leewayhertz.com>
2021-09-30 18:09:09 +0530

    Netflix

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleshow(true)
+      } else handleshow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll")
+    }
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-CRrPMPLo9vTBx5BgdCV8iKE-Ix7soG3VJQ&usqp=CAU"
+        alt="Netflix Logo"
+      />
+    </div>
+  )
+}
+
+export default Nav
